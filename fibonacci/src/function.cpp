0f2ca6d92dbdd53c7a0c952222bdf2080c97037e
soares515 <jaksonsoaresf@gmail.com>
2021-10-24 14:38:45 -0300

    Revert "Conclusão"
    This reverts commit b1424b577a26efbef9192eea8c46674b5b35f8e0.

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,16 +1,9 @@
 #include "function.h"
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
-  {
-    unsigned int a1 = 0, a2 = 1, aux;
+{
+    // TODO: adicione o seu código aqui.
 
-    std::vector<unsigned int> vetor;
-    while( a2 < n )
-      {
-        vetor.push_back(a2);
-        aux = a2;
-        a2 += a1;
-        a1 = aux;
-      } 
-    return vetor;
-  }
+    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
+    return std::vector<unsigned int>{};
+}
