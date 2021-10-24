b1424b577a26efbef9192eea8c46674b5b35f8e0
soares515 <jaksonsoaresf@gmail.com>
2021-10-24 14:13:20 -0300

    Conclusão

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,16 @@
 #include "function.h"
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
-{
-    // TODO: adicione o seu código aqui.
+  {
+    unsigned int a1 = 0, a2 = 1, aux;
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
-}
+    std::vector<unsigned int> vetor;
+    while( a2 < n )
+      {
+        vetor.push_back(a2);
+        aux = a2;
+        a2 += a1;
+        a1 = aux;
+      } 
+    return vetor;
+  }
